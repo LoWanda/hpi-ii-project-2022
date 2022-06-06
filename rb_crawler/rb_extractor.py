98befd4415bb2e300f1fbbd7583890c08d9cec24
susanilling <susan.illing@uni-potsdam.de>
2022-06-06 17:54:36 +0200

    modify for separate producers

--- rb_crawler/rb_extractor.py
+++ rb_crawler/rb_extractor.py
@@ -4,8 +4,10 @@ from time import sleep
 import requests
 from parsel import Selector
 
-from build.gen.bakdata.corporate.v1.corporate_pb2 import Corporate, Status
-from rb_producer import RbProducer
+from build.gen.bakdata.corporate.v2.corporate_pb2 import Corporate
+from build.gen.bakdata.announcement.announcement_pb2 import Announcement, Status
+from rb_producer_c import RbProducerC
+from rb_producer_a import RbProducerA
 
 import re
 
@@ -16,7 +18,8 @@ class RbExtractor:
     def __init__(self, start_rb_id: int, state: str):
         self.rb_id = start_rb_id
         self.state = state
-        self.producer = RbProducer()
+        self.producer_c = RbProducerC()
+        self.producer_a = RbProducerA()
 
     def extract(self):
         while True:
@@ -28,15 +31,18 @@ class RbExtractor:
                     break
                 selector = Selector(text=text)
                 corporate = Corporate()
-                corporate.rb_id = self.rb_id
-                corporate.state = self.state
+                announcement = Announcement()
+                announcement.rb_id = self.rb_id
+                announcement.state = self.state
                 corporate.reference_id = self.extract_company_reference_number(selector)
+                announcement.reference_id = self.extract_company_reference_number(selector)
                 event_type = selector.xpath("/html/body/font/table/tr[3]/td/text()").get()
-                corporate.event_date = selector.xpath("/html/body/font/table/tr[4]/td/text()").get()
+                announcement.event_date = selector.xpath("/html/body/font/table/tr[4]/td/text()").get()
                 corporate.id = f"{self.state}_{self.rb_id}"
+                announcement.id = f"{self.rb_id}_{self.state}"
                 raw_text: str = selector.xpath("/html/body/font/table/tr[6]/td/text()").get()
-                self.handle_text(corporate, raw_text)
-                self.handle_events(corporate, event_type, raw_text)
+                self.handle_corporates(corporate, raw_text)
+                self.handle_events(announcement, event_type, raw_text)
                 self.rb_id = self.rb_id + 1
                 log.debug(corporate)
             except Exception as ex:
@@ -56,26 +62,28 @@ class RbExtractor:
     def extract_company_reference_number(selector: Selector) -> str:
         return ((selector.xpath("/html/body/font/table/tr[1]/td/nobr/u/text()").get()).split(": ")[1]).strip()
 
-    def handle_events(self, corporate, event_type, raw_text):
+    def handle_events(self, announcement, event_type, raw_text):
         if event_type == "Neueintragungen":
-            self.handle_new_entries(corporate, raw_text)
+            self.handle_new_entries(announcement, raw_text)
         elif event_type == "Veränderungen":
-            self.handle_changes(corporate, raw_text)
+            self.handle_changes(announcement, raw_text)
         elif event_type == "Löschungen":
-            self.handle_deletes(corporate)
+            self.handle_deletes(announcement)
             
     # extract information from raw_text
     # needs modifications for other states than bw
-    def handle_text(self, corporate, raw_text):
+    def handle_corporates(self, corporate: Corporate, raw_text: str) -> Corporate:
         search_name = '^\"?(.*?)\"?,\s([A-zÜüÖöÄä-]+?[^A-Z]\s)*?\('
         search_address1 = '.*?\((.+?,\s\d{5}\s.+?)[,\)].*\.'
         search_address2 = '.*?\((.+?)\)\.'
+        search_address3 = '(.*?),\s(\d+)\s(.*)'
+        search_address4 = '(\d+)\s(.*)'
         search_capital = 'Stammkapital:\s(.*?EUR)'
         search_industry1 = '.*?\(.+?,\s\d{5}\s.+?,\s?(.*?)?\.?\)\.'
         search_industry2 = 'Gegenstand:\s([Ss]ind\s)?([Ii]st\s)?(.*?)\.\s[A-ZÖÄÜ]'
         search_ceo = '[^r][\s\.]Geschäftsführer(in)?:\s([^:]*?\d{4})[,\.]'
         search_ceo_name = '(.*?),[^,]*?,[^,]*?\d'
-        replace_text = '.*?\)\.\s?'
+        #replace_text = '.*?\)\.\s?'
         
         if re.search(search_name, raw_text):
             corporate.name = re.search(search_name, raw_text).group(1)
@@ -83,11 +91,21 @@ class RbExtractor:
             pass
         
         if re.search(search_address1, raw_text):
-            corporate.address = re.search(search_address1, raw_text).group(1)
+            address = re.search(search_address1, raw_text).group(1)
         elif re.search(search_address2, raw_text):
-            corporate.address = re.search(search_address2, raw_text).group(1)
+            address = re.search(search_address2, raw_text).group(1)
         else:
             pass
+            
+        if re.search(search_address3, address):
+            corporate.street = re.search(search_address3, address).group(1)
+            corporate.postal_code = re.search(search_address3, address).group(2)
+            corporate.city = re.search(search_address3, address).group(3)
+        elif re.search(search_address4, address):
+            corporate.postal_code = re.search(search_address4, address).group(1)
+            corporate.city = re.search(search_address4, address).group(2)
+        else:
+            corporate.city = address
         
         if re.search(search_capital, raw_text):
             corporate.capital = re.search(search_capital, raw_text).group(1)
@@ -111,25 +129,26 @@ class RbExtractor:
         else:
             pass
         
-        corporate.information = re.sub(replace_text, '', raw_text)
+        #raw_text = re.sub(replace_text, '', raw_text)
+        self.producer_c.produce_to_topic(corporate=corporate)
 
-    def handle_new_entries(self, corporate: Corporate, raw_text: str) -> Corporate:
-        log.debug(f"New company found: {corporate.id}")
-        corporate.event_type = "create"
-        #corporate.information = raw_text
-        corporate.status = Status.STATUS_ACTIVE
-        self.producer.produce_to_topic(corporate=corporate)
+    def handle_new_entries(self, announcement: Announcement, raw_text: str) -> Announcement:
+        log.debug(f"New company found: {announcement.id}")
+        announcement.event_type = "create"
+        announcement.information = raw_text
+        announcement.status = Status.STATUS_ACTIVE
+        self.producer_a.produce_to_topic(announcement=announcement)
 
-    def handle_changes(self, corporate: Corporate, raw_text: str):
-        log.debug(f"Changes are made to company: {corporate.id}")
-        corporate.event_type = "update"
-        corporate.status = Status.STATUS_ACTIVE
-        #corporate.information = raw_text
-        self.producer.produce_to_topic(corporate=corporate)
+    def handle_changes(self, announcement: Announcement, raw_text: str):
+        log.debug(f"Changes are made to company: {announcement.id}")
+        announcement.event_type = "update"
+        announcement.status = Status.STATUS_ACTIVE
+        announcement.information = raw_text
+        self.producer_a.produce_to_topic(announcement=announcement)
 
-    def handle_deletes(self, corporate: Corporate):
-        log.debug(f"Company {corporate.id} is inactive")
-        corporate.event_type = "delete"
-        corporate.information = ""
-        corporate.status = Status.STATUS_INACTIVE
-        self.producer.produce_to_topic(corporate=corporate)
+    def handle_deletes(self, announcement: Announcement):
+        log.debug(f"Company {announcement.id} is inactive")
+        announcement.event_type = "delete"
+        announcement.information = ""
+        announcement.status = Status.STATUS_INACTIVE
+        self.producer_a.produce_to_topic(announcement=announcement)

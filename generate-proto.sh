a11adb6480fd2378a3853e00973c7698c421039d
susanilling <susan.illing@uni-potsdam.de>
2022-06-06 17:46:07 +0200

    add proto files

--- generate-proto.sh
+++ generate-proto.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-protoc --proto_path=proto --python_out=build/gen proto/bakdata/corporate/v1/corporate.proto
+protoc --proto_path=proto --python_out=build/gen proto/bakdata/corporate/v2/corporate.proto
+protoc --proto_path=proto --python_out=build/gen proto/bakdata/announcement/announcement.proto
 protoc --proto_path=proto --python_out=build/gen proto/dpma/patent.proto
